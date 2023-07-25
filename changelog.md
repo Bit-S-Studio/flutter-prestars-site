ccab3c6c372ae1b5324087d01bb3b7644f1fc265
lucasdk3 <lucas.batista@midway.com.br>
2023-07-25 11:34:31 -0300

    Ajustado botão increva-se para inscreva-se

--- changelog.md
+++ changelog.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Ajustado frase do segundo Flex do atleta
 - Ajustado bug de overflow na UiTags
 - Removido obrigatóriedade da altura do pai
+- Ajustado botão increva-se para inscreva-se
 
 ### Changed
 
--- lib/app/src/ui/components/molecules/ui_header.dart
+++ lib/app/src/ui/components/molecules/ui_header.dart
@@ -3,28 +3,32 @@ import 'package:flutter/material.dart';
 import '../../../../../prestars_exports.dart';
 
 class UiHeader {
-  static PreferredSizeWidget call(BuildContext context, ScrollController scrollController) => AppBar(
+  static PreferredSizeWidget call(
+          BuildContext context, ScrollController scrollController) =>
+      AppBar(
         backgroundColor: ThemeService.colors.background,
         elevation: 0,
         toolbarHeight: 80,
         centerTitle: false,
         title: const UiLogo(),
         actions: [
-           context.isDesktop ?Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: UiButton(
-                width: 200,
-                onPressed: () {
-                  scrollController.animateTo(
-                    scrollController.position.maxScrollExtent,
-                    duration: const Duration(
-                        milliseconds:
-                            500), // Duração da animação em milissegundos
-                    curve: Curves.easeInOut, // Curva de animação
-                  );
-                },
-                label: 'Increva-se'),
-          ) : const SizedBox()
+          context.isDesktop
+              ? Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: UiButton(
+                      width: 200,
+                      onPressed: () {
+                        scrollController.animateTo(
+                          scrollController.position.maxScrollExtent,
+                          duration: const Duration(
+                              milliseconds:
+                                  500), // Duração da animação em milissegundos
+                          curve: Curves.easeInOut, // Curva de animação
+                        );
+                      },
+                      label: 'Inscreva-se'),
+                )
+              : const SizedBox()
         ],
       );
 }
